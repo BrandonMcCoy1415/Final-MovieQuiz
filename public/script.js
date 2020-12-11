0f989a30d3d9adc6f765e68ab46710439c6b12f7
Allison Siegel <asiegel1@umd.edu>
2020-12-11 16:44:55 -0500

    added library with methods

--- public/script.js
+++ public/script.js
@@ -39,7 +39,6 @@ function getSelectedCheckboxValues(type) {
         values.push(checkbox.value);
     });
     return values;
-
 }
 const preferences = getSelectedCheckboxValues('checkbox')
 console.log(preferences)
